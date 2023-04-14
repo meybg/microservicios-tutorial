b4c93e5a5088e840d112dddae0f515f2855d96a4
meybg <mabelita1975@gmail.com>
2023-04-13 20:52:54 -0500

    uno

---
+++ carro-service/src/main/java/com/carro/service/entidades/Carro.java
@@ -0,0 +1,59 @@
+package com.carro.service.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Carro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String marca;
+	private int usuarioId;
+	private String modelo;
+	
+	
+
+	public Carro() {
+		super();
+	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getUsuarioId() {
+		return usuarioId;
+	}
+
+	public void setUsuarioId(int usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
